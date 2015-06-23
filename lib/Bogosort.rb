7e7351401d1ce124fb3d95541a6cf5ed415d46fb
284km <at284km@gmail.com>
2015-06-24 00:21:48 +0900

    Implements

--- lib/Bogosort.rb
+++ lib/Bogosort.rb
@@ -3,3 +3,20 @@ require "Bogosort/version"
 module Bogosort
   # Your code goes here...
 end
+
+class Array
+  def bogosort
+    until self.sorted?
+      self.shuffle!
+    end
+    self
+  end
+
+  def sorted?
+    return true if self.size < 2
+    1.upto(self.size - 1) do |i|
+      return false unless self[i - 1] <= self[i]
+    end
+    true
+  end
+end
